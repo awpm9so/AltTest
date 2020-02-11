f539d0f624b81e8260e646480e6b47bdde71e5f9
awpm9so <awpm9so@gmail.com>
2020-02-11 23:17:18 +0300

    add addEventListener to the button "Add" and the ability to delete records

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-function add(){
-    var li = document.createElement("li");  
-    var input = document.getElementById("add");
-    li.innerHTML = input.value;
-   
+
+window.onload=function(){
+//добавляем знечение введное в текстовое поле в список 
+var button = document.getElementById("add_btn");
+button.addEventListener("click", function(){
+    
+    var li = document.createElement("li"); 
+    var input = document.getElementById("add_input");
+    li.innerHTML = input.value + " <button style=\"font-size:5px\">X</button>"; 
     input.value = "";
-
     document.getElementById("list").appendChild(li);
+
+
+     //удаление записи по нажатию на кнопку "X", которая распологаяется рядом с ней
+     li.addEventListener('click', function (e) {
+                if(e.target && e.target.nodeName == "BUTTON") {                     
+                e.target.parentNode.remove();               
+        }         
+        });
+
+}); 
 }
-
-
 
